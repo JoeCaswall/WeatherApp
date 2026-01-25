470d253eaa1d2c17c1062900a670b8b90656301d
JoeCaswall <joe.caswall@gmail.com>
2026-01-25 23:18:00 +0000

    added JWT utility and filter and inserted into security chain, users table is created on app launch, sign up and log in endpoints functional

---
+++ src/main/java/com/MobileApps/WeatherApp/controller/AuthController.java
@@ -0,0 +1,29 @@
+package com.MobileApps.WeatherApp.controller;
+
+import com.MobileApps.WeatherApp.dto.LoginRequest;
+import com.MobileApps.WeatherApp.dto.SignupRequest;
+import com.MobileApps.WeatherApp.entity.User;
+import com.MobileApps.WeatherApp.service.AuthService;
+import com.MobileApps.WeatherApp.service.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final UserService userService;
+    private final AuthService authService;
+
+    @PostMapping("/signup")
+    public User signup(@Valid @RequestBody SignupRequest request) {
+        return userService.registerUser(request);
+    }
+
+    @PostMapping("/login")
+    public String login(@Valid @RequestBody LoginRequest request) {
+        return authService.login(request);
+    }
+}
---
+++ src/main/java/com/MobileApps/WeatherApp/dto/SignupRequest.java
@@ -0,0 +1,18 @@
+package com.MobileApps.WeatherApp.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class SignupRequest {
+
+    @NotBlank
+    private String username;
+
+    @NotBlank
+    private String password;
+
+    @Email
+    private String email;
+}
